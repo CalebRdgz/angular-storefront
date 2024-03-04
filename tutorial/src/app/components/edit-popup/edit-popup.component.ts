7fc70aa4cb58c30f4164a29c4fba1a6f024243f6
CalebRdgz <calebrodgz@gmail.com>
2024-03-04 17:05:16 -0600

    Fixed the Add Product button on the home component by making display a double binding to take @Input and @Output emitters to change the state in onConfirm() and onCancel()

--- tutorial/src/app/components/edit-popup/edit-popup.component.ts
+++ tutorial/src/app/components/edit-popup/edit-popup.component.ts
@@ -9,13 +9,21 @@ import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule], //CommonModule Exports all the basic Angular directives and pipes(NgIf, NgForOf, DecimalPipe, etc.)
+  imports: [
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    RatingModule,
+    ButtonModule,
+  ], //CommonModule Exports all the basic Angular directives and pipes(NgIf, NgForOf, DecimalPipe, etc.)
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
   //Input to allow us to display the popup or not (default as false):
   @Input() display: boolean = false;
+  //make display into double binding with event emitter of the same type^(boolean):
+  @Output() displayChange = new EventEmitter<boolean>();
   //create the header variable input for <p-dialog>, empty as default, ! = always provide header:
   @Input() header!: string;
   //build our product, initialized as empty:
@@ -32,10 +40,13 @@ export class EditPopupComponent {
   //onConfirm function uses the confirm output^ to emit the product:
   onConfirm() {
     this.confirm.emit(this.product);
+    this.display = false;
+    this.displayChange.emit(this.display);
   }
 
   //onCancel function will just close the popup (instead of using the cancel @Output):
   onCancel() {
     this.display = false;
+    this.displayChange.emit(this.display);
   }
 }
